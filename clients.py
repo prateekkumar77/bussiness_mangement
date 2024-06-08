0a4694f43dada63a23f0e4531fd2e1d4e70cf1e3
Prateek Kumar <prateekkumar77@gmail.com>
2024-06-09 03:05:49 +0530

    new changes made to clients app

--- clients.py
+++ clients.py
@@ -1,5 +1,36 @@
 import streamlit as st
 
+clients = []
+totalClients = 117 #len(clients)
+change = 23
+
+if change < 0:
+  change = '-' + str(change)
+else:
+  change = "+" + str(change)
+
 
 def app():
   st.header("Client Data Manager")
+
+  st.metric(label="Total Clients", value=totalClients, delta=change, help="Value in color represent the change in numbers in within a week")
+
+  addClientBtn = st.button(label="Register", on_click=addClient)
+  
+
+def addClient():
+    with st.container():
+        st.markdown("<h1 style= 'text-align: center'>Client Registration<h1>", unsafe_allow_html=True)
+        with st.form("Register a Client"):
+            col1,col2 = st.columns(2)
+            name = col1.text_input(label="Name")
+            email = col2.text_input(label="Email ID")
+            phn = col1.text_input(label="Phone No.")
+            col2.checkbox(label="Subcribed")
+            add = st.text_input("Address")
+            submit1 = st.form_submit_button("Submit")
+            if submit1:
+                if name == "" or email == "" or phn == "" or add == "":
+                    st.warning("Please fill all the above fields")
+                else:
+                    st.success("Registered Successfully")
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import home ,account
+import home ,account, clients, orders
 
 #x = st.image('logo.jpg', caption='Company Logo')
 
-st.set_page_config(page_title="Test",
+st.set_page_config(page_title="Rasamrit",
                   )
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 class MultiApp:
@@ -22,10 +29,10 @@ class MultiApp:
     with st.sidebar:
       app = option_menu(menu_title='All Apps ',
                         options=[
-                            'Home', 'Account'],
+                            'Home', 'Account', 'Clients', 'Orders'],
                         icons=[
-                            'house-fill', 'person-circle'],
-                        menu_icon='chat-text-fill',
+                            'house-fill', 'person-circle', 'people-fill', 'box-seam' ],
+                        menu_icon='cast',
                         default_index=1,
                         styles={
                             "container": {
@@ -52,6 +59,11 @@ class MultiApp:
       home.app()
     if app == "Account":
       account.app()
+    if app == "Clients":
+      clients.app()
+    if app == "Orders":
+      orders.app()
+
     
 
   run()
