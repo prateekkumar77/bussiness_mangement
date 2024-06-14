f6b5413b4032de5204c95400da72860b9205c369
Prateek Kumar <prateekkumar77@gmail.com>
2024-06-15 03:26:21 +0530

    added new client model and new UI to register client Please

--- models.py
+++ models.py
@@ -1,14 +1,46 @@
 #data which is persisted in a database is defined here
+import datetime
 
 class client:
-    name:int
-    phone:int
-    address:str
-    balance:int
-    suscribed:bool
-    emailID:str
-    clientID:str
 
+    def __init__(self,name:str="",client_id:str=None,subscribed:bool=False,end_date:datetime=None,flat_no:str="",society:str="",address1:str="",email_id:str="",phn_no:int=None, plan:str="") -> None:
+        self.name = name
+        self.client_id = client_id
+        self.subscribed = subscribed
+        self.end_date = end_date
+        self.address1 = address1
+        self.flat_no = flat_no
+        self.society = society
+        self.email_id = email_id
+        self.phone_no = phn_no
+        self.plan = plan
+
+
+    def save(self):
+        pass
+
+
+    def assign_values(self,name,client_id,subscribed,end_date,flat_no,society,phn_no,email_id,plan=None,address1=""):
+        self.name = name
+        self.client_id = client_id
+        self.subscribed = subscribed
+        self.end_date = end_date
+        self.address1 = address1
+        self.flat_no = flat_no
+        self.society = society
+        self.email_id = email_id
+        self.phone_no = phn_no
+        self.plan = plan
+
+    def search_and_populate(self, client_name:str="", client_id:str=None):
+        pass
+
+    def getClientInfo(self) ->dict|None:
+        if self.client_id is not None:
+            return {}
+        else:
+            return None
+    
 
 class order:
     orderID:str
@@ -35,4 +67,5 @@ class subscription_plans:
     susbscriptions_desc:str
     default_delivery_time:str
     price:int
+
 
--- sites/register_client.py
+++ sites/register_client.py
@@ -1,16 +1,28 @@
 import streamlit as st
+from models import client
+import datetime
 
+client1 = client()
 
 def app():
         st.header("📝 :orange[Client Registration]")
         #st.markdown("<h2 style= 'text-align: center'>Client Registration<h2>", unsafe_allow_html=True)
         container1 = st.container()
         col1,col2 = container1.columns(2)
-        name = col1.text_input(label="Name")
-        email = col2.text_input(label="Email ID")
-        phn = col1.text_input(label="Phone No.")
-        col2.checkbox(label="Subcribed")
-        add = container1.text_input("Address")
+        name = col1.text_input(label="Full Name", max_chars=50,placeholder="Enter Full Name")
+        email = col2.text_input(label="Email ID",max_chars=50,placeholder="example@gmail.com")
+        phn = col1.text_input(label="Phone No.",max_chars=10,placeholder="9XX-XXXX-XX9")
+        cb1 = col2.checkbox(label="Subcribed",key="cb1")
+        plan = ""
+        if cb1:
+             c1,c2 = container1.columns(2)
+             plan = c1.selectbox(label="Select a Plan", options=[None]+["Plan1", "Plan2", "Plan3"],placeholder="Select a Plan",)
+             end_date = c2.date_input(label="Plan End-Date",min_value=datetime.date.today(),key="end_date",help="End Date should be atleast a week from now")
+             #print(plan)
+        col3,col4 = container1.columns(2)
+        flat_no = col3.text_input(label="Flat No.",max_chars=10,placeholder="Enter Flat No.")
+        society = col4.selectbox("Select Society", options=[None, "Society 1", "Society 2"])
+        add = container1.text_input("Address",max_chars=50)
         submit1 = container1.button("Submit",use_container_width=True,type="primary")
         if submit1:
                 if name == "" or email == "" or phn == "" or add == "":
