a0ff93951b52c69e2d93330c2099d8fc870aa74a
Prateek Kumar <prateekkumar77@gmail.com>
2024-06-16 01:31:15 +0530

    added logging features for database

--- database.py
+++ database.py
@@ -1,20 +1,8 @@
 from mysql import connector
-from mysql.connector import errorcode
-import logging
+from features import log_info, log_error
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-# Log to console
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-# Also log to a file
-file_handler = logging.FileHandler("app-info.log")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+i_log = log_info(__name__)
+err_log = log_error(__name__)
 
 MYSQL_ROOT_PASSWORD = 'root-pass'
 MYSQL_USER = 'root'
@@ -29,31 +17,11 @@ def get_live_db_object() -> connector.connect:
     except (connector.Error, IOError) as err:
         print("Failed to connect")
         print(err)
-        logger.error("Failed to connect to database: %s", err.errno)
+        err_log.error("Failed to connect to database: %s", err.errno)
 
-    logger.info("Connected to database: "+DATABASE_NAME+" with user: "+MYSQL_USER)
+    i_log.info("Connected to database: "+DATABASE_NAME+" with user: "+MYSQL_USER)
     print('Connection Established')
 
     return db
 
-db1 = get_live_db_object()
-
-cur = db1.cursor()
-
-sql = "INSERT INTO products (p_id, product_name, product_description, price, image_url, category) VALUES (%s, %s, %s, %s, %s, %s)"
-val = ('self.p', 'self.product_name', 'self.description', 500, 'self.img_url', 'self.category')
-
-flag = True
-try:
-    cur.execute(sql, val)
-except connector.Error as err:
-       print(err)
-       flag = False
-
-if flag:
-    print(cur.rowcount, "record inserted.")
-    #i_log.info(str(cur.rowcount) + " record inserted")
-    #db1.commit()
-    cur.close()
-    db1.commit()
-    db1.close()
+#db1 = get_live_db_object()
